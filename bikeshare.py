3986bc7d1e8d0747b1f6f66ccb61c0868efb7325
tawaah-taher <tawaah@outlook.com>
2021-09-16 16:00:16 +0300

    Adding missing question mark

--- bikeshare.py
+++ bikeshare.py
@@ -18,7 +18,7 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
-        city = input("Would you like to see data for Chicago, New York, or Washington?\n")
+        city = input("Would you like to see data for Chicago, New York, or Washington???\n")
         city = city.lower()
         if city in ['chicago', 'new york city', 'washington']:
             break
